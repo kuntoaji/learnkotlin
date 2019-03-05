eec6fb75ca076e1732cb72eee104e05af4f25561
Kunto Aji. K <kuntoaji@kaklabs.com>
2019-03-06 06:52:00 +0700

    learn infix function

---
+++ src/002-infix-function.kt
@@ -0,0 +1,28 @@
+fun main() {
+    infix fun Int.times(str: String) = str.repeat(this)
+    println(2 times "Aloha ")
+
+    val pair = "key" to "value"
+    println(pair)
+
+    infix fun String.onto(other: String) = Pair(this, other)
+    var myPair = "Aloha" onto "Baloha"
+    println(myPair)
+
+    var foo = Person("Foo")
+    var bar = Person("Bar")
+    var lan = Person("Lan")
+    foo likes bar
+    foo.likes(lan)
+}
+
+class Person(val name: String) {
+    // https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/mutable-list-of.html
+    val likedPeople = mutableListOf<Person>()
+
+    infix fun likes(other: Person) {
+        println("${this.name} likes ${other.name}")
+        likedPeople.add(other)
+        println("liked People: ${likedPeople.map { p -> p.name } }")
+    }
+}
