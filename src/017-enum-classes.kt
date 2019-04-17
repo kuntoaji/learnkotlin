b5d3605712a2c52bf22f02a89f3ae738fec2dd54
Kunto Aji. K <kuntoaji@kaklabs.com>
2019-04-17 16:56:38 +0700

    add enum classes

---
+++ src/017-enum-classes.kt
@@ -0,0 +1,30 @@
+// https://kotlinlang.org/docs/reference/enum-classes.html
+
+enum class State {
+    IDLE, RUNNING, FINISHED
+}
+
+enum class Color(val rgb: Int) {                      // 1
+    RED(0xFF0000),                                    // 2
+    GREEN(0x00FF00),
+    BLUE(0x0000FF),
+    YELLOW(0xFFFF00);
+
+    fun containsRed() = (this.rgb and 0xFF0000 != 0)
+}
+
+fun main() {
+    val state = State.RUNNING
+    val message = when(state) {
+        State.IDLE -> "It's idle"
+        State.RUNNING -> "It's running"
+        State.FINISHED -> "It's finished"
+    }
+
+    println(message)
+
+    val red = Color.RED
+    println(red)
+    println(red.containsRed())
+    println(Color.BLUE.containsRed())
+}
