494225c05348cacaa974d1cdfd6f509ae4b81069
Kunto Aji. K <kuntoaji@kaklabs.com>
2019-04-20 10:21:08 +0700

    add returning function

--- src/020-higher-order-functions.kt
+++ src/020-higher-order-functions.kt
@@ -2,11 +2,19 @@
 
 // A higher-order function is a function that takes another function as parameter and/or returns a function
 
+// taking a function as parameter
 fun calculate(x: Int, y: Int, operation: (Int, Int) -> Int): Int {
     return operation(x, y)
 }
 
 fun sumValues(x: Int, y: Int) = x + y
+
+// returning function
+fun operation(): (Int) -> Int {
+    return ::square
+}
+
+fun square(x: Int) = x * x
 
 fun main() {
     // passsing two integer values and the function argument ::sum
@@ -16,4 +24,7 @@ fun main() {
     val multiplyResult = calculate(4,5) { a, b -> a * b }
 
     println("sumResult $sumResult, multiplyResult $multiplyResult")
+
+    val func = operation()
+    println(func(2))
 }
