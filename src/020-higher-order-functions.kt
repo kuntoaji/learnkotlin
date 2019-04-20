be92c5581fa5cc12cd05a16e7134a5567671bf98
Kunto Aji. K <kuntoaji@kaklabs.com>
2019-04-20 10:15:41 +0700

    add higher-order function

---
+++ src/020-higher-order-functions.kt
@@ -0,0 +1,19 @@
+// https://kotlinlang.org/docs/reference/lambdas.html
+
+// A higher-order function is a function that takes another function as parameter and/or returns a function
+
+fun calculate(x: Int, y: Int, operation: (Int, Int) -> Int): Int {
+    return operation(x, y)
+}
+
+fun sumValues(x: Int, y: Int) = x + y
+
+fun main() {
+    // passsing two integer values and the function argument ::sum
+    val sumResult = calculate(4,5, ::sumValues)
+
+    // passing a function argument in a lambda
+    val multiplyResult = calculate(4,5) { a, b -> a * b }
+
+    println("sumResult $sumResult, multiplyResult $multiplyResult")
+}
