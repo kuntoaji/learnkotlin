debb96acbb582f04ca9ad3959278e61dd89d13b8
Kunto Aji. K <kunto.aji@tiket.com>
2019-08-18 20:48:01 +0700

    add with

---
+++ src/042-with.kt
@@ -0,0 +1,12 @@
+class Configuration(var host: String, var port: Int)
+
+fun main() {
+    val configuration = Configuration(host = "127.0.0.1", port = 3000)
+
+    with(configuration) {
+        println("$host:$port")
+    }
+
+    // instead of:
+    println("${configuration.host}:${configuration.port}")
+}
